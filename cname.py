0ecba73c51b6a6d1c72cfb8632bf5c0d035a9179
Iek <iek@iek>
2022-02-13 10:50:47 -0800

    improved parsing

--- cname.py
+++ cname.py
@@ -11,12 +11,11 @@ with open(f'hostfile.txt', 'w') as w:
             for entry in entries:
                 j = json.loads(entry)
                 if j['port'] == '443':
-                    cname = j['tls-grab']["common_name"][0]
+                    cname = str(j['tls-grab']["common_name"][0] + '\n')
                     hosts_entry = " ".join(j['tls-grab']["dns_names"])
                     hostnames = "\n".join(j['tls-grab']["dns_names"])
                     if len(hostnames) > 5:
                         ip = str(j['input'])
                         w.write(f'{ip} {cname} {hosts_entry}\n')
                         w2.write(cname)
-                        w2.write('\n')
                         w2.write(hostnames)
